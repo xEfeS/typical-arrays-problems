aad9419e92986996884c2910c2c24313dabee44d
xEfes <artiom.kruk.97@mail.ru>
2020-10-02 16:37:32 +0300

    feat:add min,max,avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+      return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) return 0;
+      return Math.max(...array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) return 0;
+        return array.reduce((sum, item) =>sum + item, 0) / array.length
 }
